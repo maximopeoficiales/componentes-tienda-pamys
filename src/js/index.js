89db71bf434cc901fb0ea8194501f2097c4193ee
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-02-02 02:50:44 -0500

    falta banner de contact

--- src/js/index.js
+++ src/js/index.js
@@ -5,14 +5,37 @@ document.addEventListener('DOMContentLoaded', function () {
         pauseOnHover: false,
         cover: true
     }).mount();
-    new Splide('.splide2', {
+    new Splide('.splide1', {
         // lazyload: true,
         // preloadPages: 3,
         margin: "auto",
         cover: true,
         // height: '8rem',
         padding: '1rem',
+        perPage: 1,
+        arrows: false,
+        autoWidth: true,
+    }).mount();
+    new Splide('.splide2', {
+        margin: "auto",
+        cover: true,
+        padding: '1rem',
         perPage: 2,
+        autoWidth: true,
+    }).mount();
+    new Splide('.splide3', {
+        margin: "auto",
+        cover: true,
+        padding: '1rem',
+        perPage: 2,
+        autoWidth: true,
+    }).mount();
+    new Splide('.splide4', {
+        margin: "auto",
+        cover: true,
+        padding: '1rem',
+        perPage: 2,
+        arrows: false,
         autoWidth: true,
     }).mount();
 });
